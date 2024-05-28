8a2f817457e3d60bfebce1ce6d65e54f0a47d82b
atuldemond <atuldemond@gmail.com>
2024-05-28 11:59:41 +0530

    working on redux

--- Basic-reducer-structure/src/nav/Routing.jsx
+++ Basic-reducer-structure/src/nav/Routing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import Data from "../components/Data";
+import Products from "../components/Products";
 
 const Routing = () => {
   return (
@@ -19,10 +20,18 @@ const Routing = () => {
         >
           Data
         </Link>
+
+        <Link
+          className="bg-red-200 px-[2vw] py-[1vh] hover:bg-yellow-200"
+          to="/products"
+        >
+          Products
+        </Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/data" element={<Data />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );
