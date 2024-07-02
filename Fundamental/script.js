2f3e5ed0cecc9dea508cfd3e73885ee46e41c3b3
atuldemond <atuldemond@gmail.com>
2024-07-02 11:11:21 +0530

    lecture-1

---
+++ Fundamental/script.js
@@ -0,0 +1,34 @@
+import { createStore } from "redux";
+
+const initialState = {
+  post: 0,
+};
+
+const INCREASE = "post/increase";
+const DECREASE = "post/decrease";
+const INCREASE_BY = "post/increase_by";
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return { ...state, post: state.post + 1 };
+    case DECREASE:
+      return { ...state, post: state.post - 1 };
+    case INCREASE_BY:
+      return { ...state, post: state.post + action.payload };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+console.log(store);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: INCREASE });
+store.dispatch({ type: DECREASE });
+store.dispatch({ type: INCREASE_BY, payload: 10 });
+store.dispatch({ type: INCREASE_BY, payload: 10 });
