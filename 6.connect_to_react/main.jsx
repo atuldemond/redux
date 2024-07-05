7a59b17e98026d827fdcc6e08bfe373cc472c37e
atuldemond <atuldemond@gmail.com>
2024-07-05 11:45:14 +0530

    connecting react to redux and geting data

--- 6.connect_to_react/main.jsx
+++ 6.connect_to_react/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { store } from "./reducer/script";
+
 createRoot(document.querySelector("#root")).render(
   <Provider store={store}>
     <App />
